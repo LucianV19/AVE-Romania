edb613f15d69ea59c04eddc2700fde4f7318a9ae
LucianV19 <veselin.lucian@gmail.com>
2026-05-21 13:15:39 +0300

    update the repo

--- components/DashboardView.tsx
+++ components/DashboardView.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-// This component is no longer in use and has been replaced by LeaderboardView.
+//  This component is no longer in use and has been replaced by LeaderboardView.
 // It is kept to avoid breaking imports if referenced somewhere unexpectedly,
 // but its content has been removed.
 const DashboardView: React.FC = () => {
